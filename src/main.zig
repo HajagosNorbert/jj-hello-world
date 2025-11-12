0d71d04a7f9f36c12407bb15199adb841a51b4dd
Norbert Hajagos <hajagosnorbi@gmail.com>
2025-11-12 10:00:32 +0100

    add more comments

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const steve_tutorial = @import("steve_tutorial");
 
 // Adding some comments
+// mooore.
+// Need more comments!
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
